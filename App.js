a40195cdeddb73ad3f9c2c7b9c49177aa0bf9fab
Donny Pham <Donny1Pham@yahoo.com>
2020-09-27 08:29:25 -0500

    Web Button Interaction Test
    Added a web button to test the waters

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
@@ -8,6 +8,12 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <Text> omegalul but lets see if we can do something</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress = {() => Alert.alert(":O YOU PUSHED ME!?!?!")}
+        title="Press me!!"
+        color="#0f0f0f"
+        accessibilityLable=""
+      />
     </View>
   );
 }
