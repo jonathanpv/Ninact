7878d03ccde6bd178758eca98053aa75225f90aa
subodhdhakal <subodhdhakal1000@gmail.com>
2020-09-30 17:53:12 -0500

    Added Friend list and leaderboard icons/buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './scenes/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text> omegalul but lets see if we can do something</Text>
-      <StatusBar style="auto" />
-      <Button
-        onPress = {() => Alert.alert(":O YOU PUSHED ME!?!?!")}
-        title="Press me!!"
-        color="#0f0f0f"
-        accessibilityLable=""
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen
   },
-});
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "Ninact Home Page"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <Text> omegalul but lets see if we can do something</Text>
+//       <StatusBar style="auto" />
+//       <Button
+//         onPress = {() => Alert.alert(":O YOU PUSHED ME!?!?!")}
+//         title="Press me!!"
+//         color="#0f0f0f"
+//         accessibilityLable=""
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/scenes/HomeScreen.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Text, StyleSheet,  View, TouchableOpacity, ImageBackground} from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+        <View>
+            <ImageBackground 
+                source={require('../assets/friendlisticon.png' )}
+                style={{ width: 200, height: 200}}>
+                <TouchableOpacity
+                    style={styles.friendListButtonStyle}
+                    // onPress={onPress}
+                >
+                    <Text style={{color: "black"}}>Go</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+        </View>
+        <View>
+            <ImageBackground 
+                source={require('../assets/leaderboardicon.png' )}
+                style={{ width: 200, height: 200 }}>
+                <TouchableOpacity
+                    style={styles.leaderboardButtonStyle}
+                    // onPress={onPress}
+                >
+                    <Text style={{color: "white"}}>Go</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+        </View>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex", 
+    flexDirection: "row" 
+  },
+  friendListButtonStyle: {
+    position: "absolute",
+    flex: 1,
+    borderWidth: 1,
+    height: 30,
+    width: "30%",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "white",
+    marginTop: 170,
+    marginLeft: 130
+  },
+  leaderboardButtonStyle: {
+    position: "absolute",
+    flex: 1,
+    borderWidth: 1,
+    height: 30,
+    width: "30%",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "black",
+    marginTop: 170,
+    marginLeft: 130
+  }
+  
+});
+
+export default HomeScreen;
+
