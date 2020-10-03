9562ca8fc2ee549e9c85568cad07f1e16c27f048
subodhdhakal <subodhdhakal1000@gmail.com>
2020-10-03 15:09:51 -0500

    Steal and Collaboration Button Added

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const Play = () => {
     return (
-        <View>
-            <Text style={styles.text}>Play Page</Text>
-            <Text>Not implemented</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Steal or Collaborate</Text>
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+                    <Image source={require('../assets/stealbtn.png')} style={{ width: 300, height: 63 }} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+                    <Image source={require('../assets/collabbtn.png')} style={{ width: 300, height: 63 }} />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     text: {
-        fontSize: 50
-    }
+        fontSize: 40
+    },
+    button: {
+        backgroundColor: '#8E97FD',
+        borderRadius: 20,
+        padding: 10,
+        marginBottom: 20,
+        marginTop: 20,
+        shadowColor: '#8E97FD',
+        shadowOffset: { width: 0, height: 5 },
+        shadowRadius: 10,
+        shadowOpacity: 0.35,
+      }
 });
 
 export default Play;
