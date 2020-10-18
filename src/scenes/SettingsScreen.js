81fafe9f88824a1d1450e9f8b79f7a1fe7e94a2b
subodhdhakal <subodhdhakal1000@gmail.com>
2020-10-17 21:54:54 -0500

    Sign Out Button Added]

--- src/scenes/SettingsScreen.js
+++ src/scenes/SettingsScreen.js
@@ -2,21 +2,29 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-
+import firebase from "../firebase";
 
 const SettingsScreen = ({ navigation} ) => {
     return (
         <View>
             <View>
-                <Text style={styles.text}>Settings Page text</Text>
-                <Text>Not implemented</Text>
+                <Text>Username: put user.name</Text>
+                <Text>Password: put user.password</Text>
             </View>
-            <View>
-                <Button
-                    title="Return to Home"
-                    onPress={() => navigation.navigate('HomeScreen')}
-                    color="#8E97FD"
-                />
+            <View style={styles.button}>
+                <View style={{ paddingBottom: 50, width: 200}}>
+                    <Button
+                        title="Return to Home"
+                        onPress={() => navigation.navigate('HomeScreen')}
+                        color="#8E97FD"
+                    />
+                </View>
+                <View>
+                    <Button
+                        title="Sign Out"
+                        color="#8E97FD"
+                    />
+                </View>
             </View>
         </View>
     );
@@ -28,9 +36,13 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 300
     }
 });
 
