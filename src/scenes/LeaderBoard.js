f61a4d672bf4336b249f362699832de12c427cf8
Subodh <44824816+subodhdhakal@users.noreply.github.com>
2020-12-02 15:55:47 -0600

    Leaderboard Integerated with Mutliplayer

--- src/scenes/LeaderBoard.js
+++ src/scenes/LeaderBoard.js
@@ -4,39 +4,59 @@ import React, {useEffect, useState} from 'react';
 import { View, Text, StyleSheet,  StatusBar, Image } from 'react-native';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import DefaultBackground from '../assets/components/atoms/DefaultBackground';
-import firebase from '../firebase';
+import fire from '../firebase';
 
 
 let DATA = [];
-firestore()
-  .collection('leaderboard')
-  .get()
-  .then(querySnapshot => {
-    console.log('Total users: ', querySnapshot.size);
-    querySnapshot.forEach(documentSnapshot => {
-        
-        console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
-    //   firebase.firestore()
-    //     .collection("user")
-    //     .doc(firebase.auth().currentUser.uid.toString())
-    //     .get()
-    //     .then(doc1 => {
-    //         console.log(doc1.data());
-    //         dbImage = doc1.data().imageuri;
-    //   });
+fire
+    .database()
+    .ref(`/users`)
+    .once('value')
+    .then((snapshot) => {
+        console.log(snapshot.val());
+        let json = snapshot.val()
+        let obj = {};
+        Object.keys(json).forEach(function(key){
+            obj = (json[key]);
+            DATA.push(obj)
+        })
+        sortData();
 
-    // Pull the leaderboard info from DB and create/store into obj to push it into DATA array
-    // obj = {id: ..., username: ...., score: ....}
-    const obj = {id: documentSnapshot.id, username: documentSnapshot.data().name, score: documentSnapshot.data().score, image: require('../assets/avatar.png')};
-    console.log(obj);
-    // Push the object into the DATA array
-    DATA.push(obj);
-    // Sort the DATA array using a comparator
-    sortData();
-    // Sorted DATA array limited to only 5 records (slice)
-    DATA = DATA.slice(0, 5);
-    });
-});
+        DATA = DATA.slice(0, 3)
+
+        console.log(DATA)
+
+    })
+
+// firestore()
+//   .collection('leaderboard')
+//   .get()
+//   .then(querySnapshot => {
+//     console.log('Total users: ', querySnapshot.size);
+//     querySnapshot.forEach(documentSnapshot => {
+        
+//         console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+//     //   firebase.firestore()
+//     //     .collection("user")
+//     //     .doc(firebase.auth().currentUser.uid.toString())
+//     //     .get()
+//     //     .then(doc1 => {
+//     //         console.log(doc1.data());
+//     //         dbImage = doc1.data().imageuri;
+//     //   });
+
+//     // Pull the leaderboard info from DB and create/store into obj to push it into DATA array
+//     // obj = {id: ..., username: ...., score: ....}
+//     const obj = {id: documentSnapshot.id, username: documentSnapshot.data().name, score: documentSnapshot.data().score, image: require('../assets/avatar.png')};
+//     console.log(obj);
+//     // Push the object into the DATA array
+//     DATA.push(obj);
+//     // Sort the DATA array using a comparator
+//     sortData();
+//     // Sorted DATA array limited to only 5 records (slice)
+//     DATA = DATA.slice(0, 5);
+//     });
+// });
 
 // Comparator to sort the DATA array of list based on the score. (Higher to Lower)
 const sortData = () => {
@@ -48,9 +68,9 @@ const LeaderBoard = () => {
     const renderItem = ({ item }) => (
         <View style={styles.container2}>
             <Text style={styles.title}>
-                <Image source={item.image} style={{width: 30, height: 30}} />
+                <Image source={require('../assets/avatar.png')} style={{width: 30, height: 30}} />
                 <Text style={styles.nameStyle}>Name: {item.username} <br/></Text> 
-                <Text style={styles.scoreStyle}>Score: {item.score} </Text>
+                <Text style={styles.scoreStyle}>Score: {item.points} </Text>
             </Text>
             <View
                 style={{
